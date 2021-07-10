d7b21327626a7d2070d9d958f2e9758744fb8dbe
Maksim <maksimtrofimov13@gmail.com>
2021-07-10 18:09:36 +0300

    add routes, refactoring bookPage component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -8,6 +8,9 @@ import ErrorMessage from '../errorMessage';
 import GotService from '../../services/gotService';
 import BookPage from '../pages/bookPage';
 import HousePage from '../pages/housePage';
+import BooksItem from '../pages/booksItem';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import img from './img/got.jpeg';
 
 const ToggleButton = styled.button`
 margin: 20px auto;
@@ -18,7 +21,24 @@ display: block;
 box-sizing: border-box;
 padding: 10px 20px;
 font-weight: bold;
-`
+`;
+
+const MainTitle = styled.h1`
+margin: 0 auto;
+text-align: center;
+color: #fff;
+padding: 20px;
+box-sizing: border-box;
+border: 1px solid #fff;
+`;
+
+const AppContainer = styled.div`
+overflow-x: hidden;
+background: url(${img}) center center no-repeat;
+background-size: cover;
+font-size: 16px;
+height: 100%;	
+`;
 
 
 export default class App extends Component  {
@@ -62,22 +82,30 @@ export default class App extends Component  {
         }
 
         return (
-            <> 
-                <Container>
-                    <Header />
-                </Container>
-                <Container>
-                    <Row>
-                        <Col lg={{size: 5, offset: 0}}>
-                            {RandomBlock}
-                            <ToggleButton onClick={this.onToggleRandomCharacter} type='button'>Toggle random character</ToggleButton>
-                        </Col>
-                    </Row>
-                    <CharacterPage/>
-                    <BookPage/>
-                    <HousePage/>
-                </Container>
-            </>
+           <Router>
+                <AppContainer> 
+                    <Container>
+                        <Header />
+                    </Container>
+                    <Container>
+                        <Row>
+                            <Col lg={{size: 5, offset: 0}}>
+                                {RandomBlock}
+                                <ToggleButton onClick={this.onToggleRandomCharacter} type='button'>Toggle random character</ToggleButton>
+                            </Col>
+                        </Row>
+                        <Route path='/' exact component={() => <MainTitle>Welcome to Game Of Thrones Data Base</MainTitle>} />
+                        <Route path='/characters/' component={CharacterPage} />
+                        <Route path='/houses/' component={HousePage} />
+                        <Route path='/books/' exact component={BookPage} />
+                        <Route path='/books/:id' render={
+                            ({match}) => {
+                                const {id} = match.params;
+                            return <BooksItem bookId={id} />}
+                        }/>
+                    </Container>
+                </AppContainer>
+           </Router>
         )
     }
         
--- src/components/pages/bookPage/bookPage.js
+++ src/components/pages/bookPage/bookPage.js
@@ -2,16 +2,14 @@ import React, {Component} from 'react';
 import GotService from '../../../services/gotService';
 import ErrorMessage from '../../errorMessage';
 import ItemList from '../../itemList';
-import ItemDetails, {Field} from '../../itemDetails';
-import RowBlock from '../../rowBlock';
+import {withRouter} from 'react-router-dom';
 
 
-export default class BookPage extends Component {
+class BookPage extends Component {
 
 	gotService = new GotService();
 
 	state = {
-		selectedBook: 7,
 		error: false
 	}
 
@@ -22,40 +20,24 @@ export default class BookPage extends Component {
         })
     }
 
-	onItemSelected = (id) => {
-        this.setState({
-            selectedBook: id
-        })
-    }
-
 	render() {
-
-		const {selectedBook} = this.state;
 
 		if (this.state.error) {
             return <ErrorMessage/>
         }
 
-		const itemList = (
-			<ItemList
-					 onItemSelected={this.onItemSelected}
-					 getData={this.gotService.getAllBooks}
-					 renderItem={({name}) => name}/>
-		)
-
-		const bookDetails = (
-			<ItemDetails itemId={selectedBook}
-			 category='book'
-			 getData={this.gotService.getBook}>
-				<Field field='numberOfPages' label='Number of pages'/>
-				<Field field='publiser' label='Publiser'/>
-				<Field field='released' label='Released'/>
-			</ItemDetails>
-		)
+	
 
 		return (
-			<RowBlock left={itemList} right={bookDetails} />
+			<ItemList
+				onItemSelected={(itemId) => {
+					this.props.history.push(itemId)
+				}}
+				getData={this.gotService.getAllBooks}
+				renderItem={({name}) => name}/>
 			
 		)
 	}
 }
+
+export default withRouter(BookPage);
